ea6f01d93ff5773967359f24d8ae14e2aecba4ef
Tanvir Hasan <87339504+tanvir27@users.noreply.github.com>
2023-04-10 12:22:14 +0600

    navbar implement using react bootstrap

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+import { Navbar, Nav, Button, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div>
-           <h1>This is header</h1> 
-        </div>
-    );
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="common-styles">
+      <Navbar open={open} expand="lg">
+        <Container>
+          <Link to="/" className="navbar-brand fw-bold fs-3">
+            Work Avenue
+          </Link>
+          <Navbar.Toggle onClick={() => setOpen(!open)} />
+          <Navbar.Collapse>
+            <Nav className="mx-auto fs-5 gap-4">
+              <Link to="/" className="nav-link">
+                Statistics
+              </Link>
+              <Link to="/" className="nav-link">
+                Applied Jobs
+              </Link>
+              <Link to="/" className="nav-link">
+                Blog
+              </Link>
+            </Nav>
+            <Nav>
+              <Button variant="primary">Start Applying</Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
