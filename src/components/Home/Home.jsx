6f3a54a037b9af1105232b0956eb9f021313da65
Tanvir Hasan <87339504+tanvir27@users.noreply.github.com>
2023-04-10 15:47:42 +0600

    banner section modified

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
-import bannerImg from "../../assets/banner.png";
+import bannerImg from "../../../public/assets/All Images/P3OLGJ1 copy 1.png";
 import "./Home.css";
 import { Link } from "react-router-dom";
 import CategoryList from "../CategoryList/CategoryList";
@@ -15,29 +15,35 @@ const Home = () => {
 
   return (
     <div>
-      <div className="common-styles mx-auto mb-5">
+      <div className="banner-section common-styles mx-auto mb-5">
         <Container>
-          <Row className="mx-auto align-items-center text-center py-md-5 py-3">
+          <Row className="mx-auto align-items-center text-center pt-md-5 pt-3">
             <Col md={6} className="text-sm-center text-md-start">
-              <h1 className="fw-bold">One Step Closer To Your </h1>
-              <h1 className="text-info fw-bold">Dream Job</h1>
-              <p className="lead">
+              <h1 className=" ">One Step </h1>
+              <h1 className="">Closer To Your </h1>
+              <h1 className="text-primary">Dream Job</h1>
+              <p className="fs-5">
                 Explore thousands of job opportunities with all the information
                 you need. Its your future. Come find it. Manage all your job
                 application from start to finish.
               </p>
               <Link to={"/"}>
-                <Button variant="primary" className="btn-lg">
+                <Button variant="primary" className="btn-lg mb-5">
                   Get Started
                 </Button>
               </Link>
             </Col>
             <Col md={6} className="">
-              <img src={bannerImg} alt="Banner" className="img-fluid" />
+              <img
+                src={bannerImg}
+                alt="Banner"
+                className="img-fluid position-relative align-bottom"
+              />
             </Col>
           </Row>
         </Container>
       </div>
+
       <div className="container my-5 text-center">
         <h1 className="fw-bold">Job Category List</h1>
         <p>
