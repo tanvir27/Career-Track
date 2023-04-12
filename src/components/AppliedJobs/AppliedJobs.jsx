893efbf63038dd1afbc69f930d780509f545f5c3
Tanvir Hasan <87339504+tanvir27@users.noreply.github.com>
2023-04-12 23:38:03 +0600

    responsive applied job page

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -54,15 +54,15 @@ const AppliedJobs = () => {
         )
         .map((product) => (
           <div className="container my-4" key={product.id}>
-            <div className="d-flex p-4 gap-5 w-full border">
-              <div className="d-flex justify-content-center align-items-center common-styles px-5">
+            <div className="row border p-3">
+              <div className="col-12 col-md-3 d-flex justify-content-center rounded-3 align-items-center common-styles p-4">
                 <img className="img-fluid" src={product.logo} alt="" />
               </div>
-              <div className="w-100">
+              <div className="col-12 col-md-9 p-4">
                 <h3>{product.title}</h3>
                 <h6 className="">{product.company}</h6>
-                <div className="d-flex justify-content-between">
-                  <div className="d-flex gap-3">
+                <div className="d-flex justify-content-between flex-wrap">
+                  <div className="d-flex flex-wrap gap-3">
                     <button type="button" className="btn btn-outline-primary">
                       {product.workType[0]}
                     </button>
@@ -70,31 +70,28 @@ const AppliedJobs = () => {
                       {product.workType[1]}
                     </button>
                   </div>
-                  <div>
+                  <div className="mt-3 mt-md-0">
                     <Link to={`/details/${product.id}`}>
-                      {" "}
-                      <button type="button" class="btn btn-primary">
+                      <button type="button" className="btn btn-primary">
                         View Details
                       </button>
                     </Link>
                   </div>
                 </div>
-                <div className="d-flex gap-5 my-3">
-                  <div>
+                <div className="d-flex flex-wrap gap-5 my-3">
+                  <div className="d-flex gap-2">
                     <span>
                       <img src={product.icon} alt="" />
-                    </span>{" "}
-                    {""}
+                    </span>
                     <span className="fw-bold text-success">
                       {product.location}
                     </span>
                   </div>
 
-                  <div>
+                  <div className="d-flex gap-2">
                     <span>
                       <img src={product.icon_2} alt="" />
-                    </span>{" "}
-                    {""}
+                    </span>
                     <span className="fw-bold text-success">
                       Salary: {product.salary}
                     </span>
